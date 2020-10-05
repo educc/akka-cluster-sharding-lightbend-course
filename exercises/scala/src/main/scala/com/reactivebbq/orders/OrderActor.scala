0c3e16c9d11fe086e63d262ce0c0efe9737dc1c2
ecacho <ecachocasas@gmail.com>
2020-10-04 19:11:14 -0500

    sharding - stateless system

--- exercises/scala/src/main/scala/com/reactivebbq/orders/OrderActor.scala
+++ exercises/scala/src/main/scala/com/reactivebbq/orders/OrderActor.scala
@@ -1,6 +1,9 @@
 package com.reactivebbq.orders
 
+import java.util.UUID
+
 import akka.actor.{Actor, ActorLogging, Props}
+import akka.cluster.sharding.ShardRegion.{ExtractEntityId, ExtractShardId}
 import akka.pattern.pipe
 
 import scala.concurrent.Future
@@ -20,22 +23,32 @@ object OrderActor {
   case class Envelope(orderId: OrderId, command: Command) extends SerializableMessage
 
   def props(repository: OrderRepository): Props = Props(new OrderActor(repository))
+
+  val entityIdExtractor: ExtractEntityId = {
+    case Envelope(orderId, command) => (orderId.value.toString, command)
+  }
+
+  val shardIdExtractor: ExtractShardId = {
+    case Envelope(orderId, _) => Math.abs(orderId.value.toString.hashCode % 30).toString
+  }
 }
 
 class OrderActor(repository: OrderRepository) extends Actor with ActorLogging {
   import OrderActor._
   import context.dispatcher
 
+  private val orderId: OrderId = OrderId(UUID.fromString(context.self.path.name))
+
   override def receive: Receive = {
-    case Envelope(orderId, OpenOrder(server, table)) =>
+    case OpenOrder(server, table) =>
       log.info(s"[$orderId] OpenOrder($server, $table)")
 
       repository.find(orderId).flatMap {
-        case Some(order) => duplicateOrder(orderId)
+        case Some(_) => duplicateOrder(orderId)
         case None => openOrder(orderId, server, table)
       }.pipeTo(sender())
 
-    case Envelope(orderId, AddItemToOrder(item)) =>
+    case AddItemToOrder(item) =>
       log.info(s"[$orderId] AddItemToOrder($item)")
 
       repository.find(orderId).flatMap {
@@ -43,7 +56,7 @@ class OrderActor(repository: OrderRepository) extends Actor with ActorLogging {
         case None => orderNotFound(orderId)
       }.pipeTo(sender())
 
-    case Envelope(orderId, GetOrder()) =>
+    case GetOrder() =>
       log.info(s"[$orderId] GetOrder")
 
       repository.find(orderId).flatMap {
